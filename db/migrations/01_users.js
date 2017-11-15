8d562f327ac622bbc26957e506e72e2728da99c1
Stephen Anderson <sa3196a@student.american.edu>
2017-11-14 22:46:08 -0800

    Migrate the users table so that user migration test passes

---
+++ db/migrations/01_users.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('user_id').primary();
+    table.string('name').notNullable().defaultTo('');
+    table.string('email').notNullable().unique();
+    table.string('image_url').notNullable().defaultTo('http://placekitten.com/200/300');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
